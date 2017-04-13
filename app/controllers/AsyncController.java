13869161a2c2501dc8603043c83d39eaf1962cf2
Prasad <prasadr@egnaroinc.com>
2017-04-13 14:16:46 +0530

    async controller added & tested

--- app/controllers/AsyncController.java
+++ app/controllers/AsyncController.java
@@ -3,11 +3,14 @@ package controllers;
 import akka.actor.ActorSystem;
 import javax.inject.*;
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
-import java.util.concurrent.Executor;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.TimeUnit;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
 import scala.concurrent.duration.Duration;
 import scala.concurrent.ExecutionContextExecutor;
 
@@ -44,7 +47,7 @@ public class AsyncController extends Controller {
      * a path of <code>/message</code>.
      */
     public CompletionStage<Result> message() {
-        return getFutureMessage(1, TimeUnit.SECONDS).thenApplyAsync(Results::ok, exec);
+        return getFutureMessage(15, TimeUnit.SECONDS).thenApplyAsync(Results::ok, exec);
     }
 
     private CompletionStage<String> getFutureMessage(long time, TimeUnit timeUnit) {
@@ -55,6 +58,22 @@ public class AsyncController extends Controller {
             exec
         );
         return future;
+    }
+
+    public CompletionStage<Result> getMe() throws InterruptedException {
+        return testMe().thenApplyAsync(Json::toJson).thenApplyAsync(Results::ok);
+    }
+
+    private CompletionStage<List> testMe() throws InterruptedException {
+        CompletableFuture<List> completableFuture = new CompletableFuture<>();
+        List<Integer> helpers = new LinkedList<>();
+        helpers.add(1);
+        helpers.add(5);
+        helpers.add(6);
+        helpers = helpers.stream().filter(s -> s%2==1).collect(Collectors.toList());
+        Thread.sleep(5000);
+        completableFuture.complete(helpers);
+        return completableFuture;
     }
 
 }
